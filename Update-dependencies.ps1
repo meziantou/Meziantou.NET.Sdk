d5d9959d525be27c6a78784ce353e8eda8f46b85
Gérald Barré <git@meziantou.net>
2025-09-03 15:53:11 -0400

    Update dependencies

---
+++ Update-dependencies.ps1
@@ -0,0 +1,4 @@
+# $env:PATH="$HOME/.dotnet/tools:$env:PATH"
+& dotnet tool update --global --no-cache depsupdater
+& depsupdater update --directory $PSScriptRoot
+dotnet run --project  $PSScriptRoot/tools/ConfigFilesGenerator/ConfigFilesGenerator.csproj
