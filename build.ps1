f4f8804b80172d742404784db704b36ad90034b6
Gérald Barré <git@meziantou.net>
2026-02-13 23:22:07 -0500

    wip

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+dotnet run --project  $PSScriptRoot/tools/ConfigFilesGenerator/ConfigFilesGenerator.csproj
 Get-ChildItem $PSScriptRoot/src/*.csproj | ForEach-Object -Parallel {
     dotnet pack $_ --output $using:PSScriptRoot/artifacts @using:args
 }
